1adabc1eade5973c5375d0134334a41e4e875ffd
Rob DiVincenzo <rob.divincenzo@gmail.com>
2023-01-06 23:43:37 -0500

    Microservices exercises: zmq sub example

---
+++ microservices/zmq-watcher-sub.js
@@ -0,0 +1,17 @@
+'use strict';
+const zmq = require('zeromq');
+
+// Create subscriber endpoint.
+const subscriber = zmq.socket('sub');
+
+// Subscribe to all messages.
+subscriber.subscribe('');
+
+// Handle messages from the publisher
+subscriber.on('message', data => {
+    const message = JSON.parse(data);
+    const date = new Date(message.timestamp);
+    console.log(`File "${message.file}" changed at ${date}`);
+});
+
+subscriber.connect("tcp://localhost:60400");
